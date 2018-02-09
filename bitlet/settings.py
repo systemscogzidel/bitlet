5245cacac6fd1fe48526c5b658b545e064577bcc
priya <systems@cogzidel.com>
2018-02-09 20:56:21 +0530

    white noise url added -priya

--- bitlet/settings.py
+++ bitlet/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'kf4+1+$6rqt*9&(8zwq9#yaje85y4i12%b7s7yx(hee!)89_x*'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [‘0.0.0.0:5000’, ‘localhost’, ‘bitlet.herokuapp.com’]
+ALLOWED_HOSTS = [‘bitlet.herokuapp.com’]
 
 
 # Application definition
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
