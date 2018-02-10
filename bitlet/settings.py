826b097efcae91ca05a5807a640e489930b1a952
priya <systems@cogzidel.com>
2018-02-10 11:28:00 +0530

    update file for bitlet Url change-priya

--- bitlet/settings.py
+++ bitlet/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'kf4+1+$6rqt*9&(8zwq9#yaje85y4i12%b7s7yx(hee!)89_x*'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [‘bitlet.herokuapp.com’]
+ALLOWED_HOSTS = ['localhost','127.0.0.1','bitlet.herokuapp.com']
 
 
 # Application definition
